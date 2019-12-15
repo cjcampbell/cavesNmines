d5acf670f27da42ffc5bce261652cc69ab5daf83
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2019-12-15 16:16:21 -0500

    placeholder data tidying

--- R/02_filter_data.R
+++ R/02_filter_data.R
@@ -1,6 +1,6 @@
 
 # Import data
-load(file.path(wd$data, "data_import.Rdata"))
+load(file.path(wd$data, "data_import.Rdata"), verbose = T)
 
 # Columns to keep regardless of match
 cols2Keep <- c(
@@ -10,19 +10,39 @@ cols2Keep <- c(
   )
 
 # Specify key words and terms.
-searchterms <- c("cave", "mine", "building", "wall", "house", "dwelling", "grotto", "well", "pit", "quarry","grotte")
+searchterms <- c("cave", "mine", "grotto", "quarry","grotte")
 
 myMatches <- lapply(searchterms,  function(term){
   row_nums <- sapply(1:ncol(dat), function(x) grep(term, dat[,x]) ) %>% 
-    unlist %>% 
-    unique %>% 
-    sort
-  col_nums <- sapply(1:nrow(dat), function(y) grep(term, dat[y,]) ) %>% 
-    unlist %>% 
-    unique %>% 
-    sort
-  return(list(row_nums, col_nums, term))
+    unlist
+  # col_nums <- sapply(1:nrow(dat), function(y) grep(term, dat[y,]) ) %>% 
+  #   unlist 
+  return(list(row_nums,# col_nums, 
+              term))
 })
 
 save(myMatches, file = file.path(wd$bin, "myMatches.Rdata"))
+ 
+# Select target rows, add identifier indicating which column has which key word
+dat <- dat %>% mutate(codeword = NA, colWithCodeword = NA)
+for(a in 1:length(myMatches)){
+  dat[  myMatches[[a]][[1]] , "codeword"]        <-  myMatches[[a]][[2]]#myMatches[[a]][[3]]
+  #dat[  myMatches[[a]][[1]] , "colWithCodeword"] <- colnames(dat)[ myMatches[[a]][[2]] ]
+}
+
+# # Bring content of colWithCodeword into new column
+# tidyDat0 <- dat %>%  dplyr::filter(!is.na(codeword))
+# for(b in 1:nrow(tidyDat0)){
+#   tidyDat0[ b , tidyDat0[ b , "colWithCodeword"] ]
+#   
+# }
+# 
+
+
+tidyDat <- dat %>% 
+  #dplyr::select( append(cols2Keep, c("codeword", "colWithCodeword" ) )) %>% 
+  dplyr::filter(!is.na(codeword)) %>% 
+  mutate_if(is.character, list(~na_if(., "<NA>"))) %>% 
+  select_if(~sum(!is.na(.)) > 0) # remove columns with only NA
+
 
