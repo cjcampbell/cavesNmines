4b40eca76c0b02d62b7b50696e8a61e99d8029a0
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2020-01-12 17:25:58 -0500

    bring in odd search data

--- R/01_import_data.R
+++ R/01_import_data.R
@@ -1,7 +1,11 @@
 
 #### Get, Clean Data ###
 dat <- lapply(list(5218544, 5218543, 2432341), function(x){
-  occ_search(taxonKey = x, return = 'data', fields = 'all', limit=10000)
+  my_search <- occ_search(
+    taxonKey = x, return = 'data', fields = 'all', limit=100000)
+  my_cit <- gbif_citation(my_search)
+  save( my_cit, file = file.path(wd$bin, paste0("rgbif_citation_", x, ".Rdata") ) )
+  return(my_search)
 }) %>% 
   plyr::ldply()
 
