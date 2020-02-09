698b15e2d6a931267ce4217536f820d51e93c621
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2020-02-09 17:45:31 -0500

    minot tidying

--- R_plots/plot_timing.R
+++ R_plots/plot_timing.R
@@ -3,26 +3,27 @@ require(dplyr)
 
 ord_species <- c("LACI", "LABO", "LANO")
 
-records_coded_tidy <- readRDS( file.path(wd$bin, "records_coded_tidy.rds") ) %>% 
+timing_records <- readRDS( file.path(wd$bin, "records_coded_tidy.rds") ) %>% 
   filter(def_alive == "Yes") %>% 
   dplyr::mutate(Month = stringr::str_trim(Month)) %>% 
   dplyr::mutate(tidy_month = if_else(Month == "7-8", as.numeric(7), if_else(Month == "Two winters", as.numeric(1), as.numeric(Month)))) %>% 
   dplyr::mutate(Species = factor(Species, levels = ord_species))
 
-records_coded_tidy %>% 
+
+timing_records %>% 
   ggplot() +
   geom_violin(aes(y = tidy_month, x = Species) ) 
 
-records_coded_tidy %>% 
+timing_records %>% 
   ggplot() +
   geom_density(aes(tidy_month, group = Species, fill = Species), alpha = 0.5) +
   scale_x_continuous(name = "Month", limits = c(0,12), expand = c(0,0)) +
   ylab("Density") +
   theme_minimal() +
-  scale_fill_viridis_d(option = "plasma") +
+  scale_fill_manual(values = rev(viridis::plasma(3))) +
   theme(
     panel.grid = element_blank(),
-    legend.position = "bottom",
+    legend.position = "right",
     text = element_text(size = 20)
   )
 
