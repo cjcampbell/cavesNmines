6cd028e159419744e8273efc1a7995f3f6fc6f70
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2020-02-11 11:13:39 -0500

    make colors work right etc

--- R_plots/plot_mapLegend.R
+++ R_plots/plot_mapLegend.R
@@ -1,5 +1,6 @@
 require(ggplot2)
-ggplot() +
+require(ggpubr)
+p <- ggplot() +
   
   geom_point(
     data = 
@@ -13,127 +14,44 @@ ggplot() +
     size = 11, alpha = 0.85, stroke = 1.6
     ) +
   scale_shape_manual(name = "Observation type", values = c(21,23)) +
-  scale_color_manual(values = rev(viridis::plasma(3)), labels = c("LACI", "LABO", "LANO")) +
+  scale_color_manual(
+    values = rev(viridis::plasma(3)),
+    labels = c(
+      expression("Hoary bat ("*italic("Lasiurus cinereus")*")"), 
+      expression("Eastern red bat ("*italic("Lasiurus borealis")*")"),
+      expression("Silver-haired bat ("*italic("Lasionycteris noctivagans")*")")
+      )
+    ) +
   
   geom_rect(
     data = 
-      data.frame(x1=c(1,3,1,5,4), x2=c(2,4,3,6,6), y1=c(1,1,4,1,3), y2=c(2,2,5,3,5), t=rep("Karst areas", 5), r=c(1,2,3,4,5)), 
+      data.frame(x1=c(1,3,1,5,4), x2=c(2,4,3,6,6), y1=c(1,1,4,1,3), y2=c(2,2,5,3,5), t=rep("USGS Karst areas", 5), r=c(1,2,3,4,5)), 
     aes(xmin=x1, xmax=x2, ymin=y1, ymax=y2, fill=t), 
     color=NA
     ) +
   scale_fill_manual(name = NULL, values = "grey90") +
   
   guides(
-    color = guide_legend(order = 1, override.aes = list(shape = 15)),
-    shape = guide_legend(order = 2),
+    color = guide_legend(order = 1, ncol = 1, override.aes = list(shape = 15, size = 8), title.position = "top"),
+    shape = guide_legend(order = 2, ncol=1, override.aes = list(size = 8), title.position = "top"),
     fill = guide_legend(order = 3)
   ) +
   
   theme_minimal() +
   theme(
-    text = element_text(size = 10)
+    text = element_text(size = 10),
+    legend.position = "bottom",
+    legend.text.align = 0,
+    axis.text = element_blank(),
+    plot.margin = unit(c(0,0,0,0), "in"),
+    panel.background = element_rect(fill = "transparent",colour = NA),
+    plot.background = element_rect(fill = "transparent",colour = NA)
   )
 
-ggsave(filename = file.path(wd$figs, "legend_example.tif"), device = "tiff", width = 2, height = 4, units = "in")
+# Extract the legend. Returns a gtable
+leg <- ggpubr::get_legend(p)
 
+# Convert to a ggplot and print
+as_ggplot(leg)
 
-
-# Approach 2 --------------------------------------------------------------
-
-library(cowplot)
-library(dplyr)
-
-theme_set(theme_minimal())
-
-allScales <-
-  c("4" = "red"
-    , "6" = "orange"
-    , "8" = "green"
-    , "0" = "blue"
-    , "1" = "black"
-    , "High MPG" = "cyan"
-    , "Other Region" = "yellow")
-
-mainPlot <-
-  ggplot(mtcars, aes(x = disp
-                     , y = mpg)) +
-  ##region for high mpg 
-  geom_rect(aes(fill = "High MPG")
-            , xmin = min(mtcars$disp)-5
-            , ymax = max(mtcars$mpg) + 2
-            , xmax = mean(range(mtcars$disp))
-            , ymin = 25
-            , alpha = 0.02) + 
-  ## test diff region
-  geom_rect(aes(fill = "Other Region")
-            , xmin = 300
-            , xmax = 400
-            , ymax = 30
-            , ymin = 25
-            , alpha = 0.02) + 
-  geom_point(aes(fill = factor(vs)),shape = 23, size = 8, alpha = 0.4) +
-  geom_point (aes(fill = factor(cyl)),shape = 21, size = 2) +
-  scale_fill_manual(values = allScales)
-
-vsLeg <-
-  (ggplot(mtcars, aes(x = disp
-                      , y = mpg)) +
-     geom_point(aes(fill = factor(vs)), shape = 23, size = 8, alpha = 0.4) +
-     scale_fill_manual(values = allScales
-                       , name = "VS")
-  ) %>%
-  ggplotGrob %>%
-  {.$grobs[[which(sapply(.$grobs, function(x) {x$name}) == "guide-box")]]}
-
-
-
-cylLeg <-
-  (ggplot(mtcars, aes(x = disp
-                      , y = mpg)) +
-     geom_point (aes(fill = factor(cyl)),shape = 21, size = 2) +
-     scale_fill_manual(values = allScales
-                       , name = "Cylinders")
-  ) %>%
-  ggplotGrob %>%
-  {.$grobs[[which(sapply(.$grobs, function(x) {x$name}) == "guide-box")]]}
-
-
-regionLeg <-
-  (ggplot(mtcars, aes(x = disp
-                      , y = mpg)) +
-     geom_rect(aes(fill = "High MPG")
-               , xmin = min(mtcars$disp)-5
-               , ymax = max(mtcars$mpg) + 2
-               , xmax = mean(range(mtcars$disp))
-               , ymin = 25
-               , alpha = 0.02) + 
-     ## test diff region
-     geom_rect(aes(fill = "Other Region")
-               , xmin = 300
-               , xmax = 400
-               , ymax = 30
-               , ymin = 25
-               , alpha = 0.02) + 
-     scale_fill_manual(values = allScales
-                       , name = "Region"
-                       , guide = guide_legend(override.aes = list(alpha = 0.4)))
-  ) %>%
-  ggplotGrob %>%
-  {.$grobs[[which(sapply(.$grobs, function(x) {x$name}) == "guide-box")]]}
-
-
-legendColumn <-
-  plot_grid(
-    # To make space at the top
-    vsLeg + theme(legend.position = "none")
-    # Plot the legends
-    , vsLeg, regionLeg, cylLeg
-    # To make space at the bottom
-    , vsLeg + theme(legend.position = "none")
-    , ncol = 1
-    , align = "v")
-
-plot_grid(mainPlot +
-            theme(legend.position = "none")
-          , legendColumn
-          , rel_widths = c(1,.25))
+ggsave(filename = file.path(wd$figs, "legend.tif"), device = "tiff", width = 8, height = 2, units = "in",  bg = "transparent")
