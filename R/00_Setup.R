a252060cae5f5fe95545aadfc2067c81a7316cab
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2021-02-08 19:22:21 -0500

    minor tweaks

--- R/00_Setup.R
+++ R/00_Setup.R
@@ -1,13 +1,19 @@
 
 # Load packages.
 library(plyr)
-library(dplyr)
-library(rgbif)
+library(tidyverse)
 library(stringr)
+library(data.table)
 
+mywd <- "/Users/cjcampbell/cavesNmines"
 # Make an object to help navigate the subdirectories.
 wd <- list()
-wd$bin  <- file.path( getwd(), "bin" )
-wd$data <- file.path( getwd(), "data" )
-wd$R    <- file.path( getwd(), "R" )
-wd$figs <- file.path( getwd(), "figs" )
+wd$bin  <- file.path( mywd, "bin" )
+wd$data <- file.path( mywd, "data" )
+wd$R    <- file.path( mywd, "R" )
+wd$figs <- file.path( mywd, "figs" )
+wd$tmp <- file.path(wd$bin, "tmp")
+
+lapply(wd, function(i) {
+  if(!dir.exists(i)) { dir.create(i) }
+})
